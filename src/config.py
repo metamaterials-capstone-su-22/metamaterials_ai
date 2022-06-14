27e58de37631b7c7a9901169ce2b099db66bc53e
Spencer Song <spencersong@berkeley.edu>
2022-06-13 20:29:56 -0700

    fixed the config back to the original

--- src/config.py
+++ src/config.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    backward_batch_size: int = 100
+    backward_batch_size: int = 2**9
     backward_lr: float = 1e-6  # tune.loguniform(1e-6, 1e-5),
-    backward_num_epochs: int = 2
+    backward_num_epochs: int = 2500
     create_plots = False
     data_file = "stainless_steel.pt"  # name of the data file
     data_folder: str = "local_data"  # Path to the data folder
-    forward_batch_size: int = 150
+    forward_batch_size: int = 2**9
     forward_lr: float = 1e-6  # tune.loguniform(1e-7, 1e-4),
-    forward_num_epochs: int = 2
+    forward_num_epochs: int = 1600
     load_forward_checkpoint: bool = True
     load_backward_checkpoint: bool = False
     model_arch = "MLPMixer"  # options 'MLPMixer'
