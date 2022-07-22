3e29d2fe7ca1121b9ce388693ac3daeaa0e24c31
pmotameni <47757728+pmotameni@users.noreply.github.com>
2022-07-22 11:48:50 -0700

    cnn branch tuned

--- src/config.py
+++ src/config.py
@@ -13,7 +13,7 @@ class Config(BaseModel):
     data_folder: str = "local_data"  # Path to the data folder
     data_portion: float = 1  # Percentage of data being used in the (0 - 1]
     direction: str = "direct"  # direct, inverse, both
-    direct_arch = "ann"  # options 'mlpmixer', 'resnet1d','ann', 'cnn,
+    direct_arch = "cnn"  # options 'mlpmixer', 'resnet1d','ann', 'cnn,
     direct_batch_size: int = None  # 2**9 512
     direct_gamma: float = .1  # schedular gamma
     direct_lr: float | None = None  # leave default to None
@@ -21,8 +21,8 @@ class Config(BaseModel):
     direct_num_epochs: int = 1600  # default 1600
     # Default= None: It should be under'{work_folder}/saved_best'
     # = "D-0.9-res-ann-inconel.ckpt"  # Default None
-    direct_saved_ckpt: str | None = 'D-1-ann-stainless.ckpt'
-    inverse_arch = "ann"  # options 'MLPMixer', 'resnet1d','ann', 'res-ann', 'cnn,
+    direct_saved_ckpt: str | None = 'D-1-cnn-stainless.ckpt'
+    inverse_arch = "cnn"  # options 'MLPMixer', 'resnet1d','ann', 'res-ann', 'cnn,
     inverse_batch_size: int = None  # 2**9 512
     inverse_gamma: float = .1  # schedular gamma
     inverse_lr: float = None  # tune.loguniform(1e-6, 1e-5)
@@ -31,7 +31,7 @@ class Config(BaseModel):
     enable_early_stopper: bool = True  # when 'True' enables early stopper
     # Default= None: It should be under'{work_folder}/saved_best'
     # str | None =  # Default: None
-    inverse_saved_ckpt: str | None = 'I-1-ann-stainless.ckpt'
+    inverse_saved_ckpt: str | None = 'I-1-cnn-stainless.ckpt'
     load_direct_checkpoint: bool = False
     load_inverse_checkpoint: bool = False
     num_gpu: int = 1  # number of GPU
