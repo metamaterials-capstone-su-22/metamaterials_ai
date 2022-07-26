33f9fcd6d6e2449a00cf8df2a3cea3b3260ee27c
Spencer Song <spencersong@berkeley.edu>
2022-07-26 01:21:33 -0700

    cant think anymore, predictor is done, completed the binning in notebook, need to verify

--- src/predictor.py
+++ src/predictor.py
@@ -23,86 +23,116 @@ SS_DATA_PATH = '../local_data/steel-onehot.pt'
 # MODEL_PATH = "data/mymodels"
 
 class Predictor:
-    def __init__(self, desired, include_inconel = True, include_stainless = True, round_laser_params = False) -> None:
-        # may have to reshape desired to 1,800 tensor
-        self.desired = desired
-        self.include_inconel = include_inconel
-        self.include_stainless = include_stainless
+    def __init__(
+        self, 
+        I_INC_CKPT_PATH, 
+        I_SS_CKPT_PATH,  
+        D_INC_CKPT_PATH, 
+        D_SS_CKPT_PATH,
+        round_laser_params = False,
+            ) -> None:
+        # Inputs: 
+        # pass in emissivity curves into 'desired' 
+
         inc_params = torch.load(Path(INC_DATA_PATH))["laser_params"]
         steel_params = torch.load(Path(SS_DATA_PATH))["laser_params"]
+
         self.round_laser_params = round_laser_params
+
+        self.I_INC_CKPT_PATH = I_INC_CKPT_PATH, 
+        self.I_SS_CKPT_PATH =I_SS_CKPT_PATH,  
+        self.D_INC_CKPT_PATH = D_INC_CKPT_PATH, 
+        self.D_SS_CKPT_PATH = D_SS_CKPT_PATH,
+
         self.ss_max_speed, self.ss_max_spacing = steel_params.max(0)[0][0].item(), steel_params.max(0)[0][1].item()
         self.ss_min_speed, self.ss_min_spacing = steel_params.min(0)[0][0].item(), steel_params.min(0)[0][1].item()
         self.inc_max_speed, self.inc_max_spacing = inc_params.max(0)[0][0].item(), inc_params.max(0)[0][1].item()
         self.inc_min_speed, self.inc_min_spacing = inc_params.min(0)[0][0].item(), inc_params.min(0)[0][1].item()
     
-    def denormalize_decode_result(self, y_hat, max_speed, max_spacing, min_speed, min_spacing):
-        """input: 1,14 tensor
-            output: 1,3 tensor with wattage no longer one hot encoded"""
-        watts = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3]
+    def denormalize_and_decode(self, y_hat, substrate, change_dimension=False):
+        """input: y_hat.shape[0], 14 tensor
+            output: y_hat.shape[0], 3 tensor with wattage no longer one hot encoded
 
-        watt_arg = torch.argmax(y_hat[0][2:])
-        y_final = torch.empty(3, dtype=torch.float32)
-        y_final[0] = y_hat[0][0] * max_speed #(max_speed - min_speed) * (y_hat[0][0] + min_speed) # TODO call the scale
-        y_final[1] = y_hat[0][1] * max_spacing#(max_spacing - min_spacing) * (y_hat[0][1] + min_spacing) # TODO call the scale
-        y_final[2]= watts[watt_arg]
+            substrate:  pick the substrate, options: "inconel", other
+            change_dimension: If you want to reverse the one-hot encoding applied to the wattage
+                This changes output size from (shape[0],14) to (shape[0],3)"""
+        if substrate == "inconel":
+            max_speed = self.inc_max_speed
+            min_speed = self.inc_min_speed
+            max_spacing = self.inc_max_spacing
+            min_spacing = self.inc_min_spacing
+        else:
+            max_speed = self.ss_max_speed
+            min_speed = self.ss_min_speed
+            max_spacing = self.ss_max_spacing
+            min_spacing = self.ss_min_spacing
+
+        watts = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3]
+        watt_arg = torch.argmax(y_hat[:,2:], axis = 1)
+        dim = 14
+        if change_dimension:
+            dim = 3
+        y_final = torch.empty((y_hat.shape[0], dim), dtype=torch.float32)
+
+        y_final[:,0] = y_hat[:,0] * (max_speed - min_speed) + min_speed
+        y_final[:,1] = y_hat[:,1]  * (max_spacing - min_spacing) + min_spacing
+        if change_dimension:
+            y_final[:,2] = torch.tensor([watts[i.item()] for i in watt_arg])
+        else:
+            y_final[:,2:] = y_hat[:,2:]
+
         if self.round_laser_params:
-            y_final[0] = torch.round(y_final[0], decimals = -1)
-            y_final[1] = torch.round(y_final[1], decimals = 0)
-        y_final = y_final.reshape(1,3)
+            y_final[:,0] = torch.round(y_final[:,0], decimals = -1)
+            y_final[:,1] = torch.round(y_final[:,1], decimals = 0)
+            
         return y_final
-    
-    def denorm_result(self, y_hat, max_speed, max_spacing, min_speed, min_spacing):
-        y_hat[0][0] = torch.round(y_hat[0][0] * max_speed, decimals = -1) #(max_speed - min_speed) * (y_hat[0][0] + min_speed) # TODO call the scale
-        y_hat[0][1] = torch.round(y_hat[0][1] * max_spacing, decimals = 0)
+
+    def run_model(self, input, substrate, direction):
+        #select correct path
+        if substrate == "inconel" and direction == "inverse":
+            path = Path(I_INC_CKPT_PATH)
+        elif substrate == "inconel" and direction == "direct":
+            path = Path(D_INC_CKPT_PATH)
+        elif substrate == "steel" and direction == "direct":
+            path = Path(D_SS_CKPT_PATH)
+        elif substrate == "steel" and direction == "inverse":
+            path = Path(I_SS_CKPT_PATH)
+        else:
+            raise Exception(f'Model path not correctly specified or does not exist at {path}!')
+
+        #select direction of model
+        if direction == "inverse":
+            model = InverseModel.load_from_checkpoint(path, strict=False)
+        elif direction == "direct":
+            model = DirectModel.load_from_checkpoint(path, strict=False)
+        else:
+            raise Exception(f'Model direction not specified {direction}!')
+
+        with torch.no_grad():
+            y_hat = model(input)
+
         return y_hat
 
-    def run_inverse(self, input):
-        x = input
-        #run INC Inverse        
-        print(os.getcwd())
-        INC_PATH = Path(I_INC_CKPT_PATH)
-        if not Path.is_file(INC_PATH):
-            raise Exception(f'Model file does not exist at {INC_PATH}!')
-        i_model_ss = InverseModel.load_from_checkpoint(INC_PATH, strict=False)
-        i_model_ss.eval()
-        with torch.no_grad():
-            y_hat_ss = i_model_ss(x)
-
-        #run SS Inverse
-        SS_PATH = Path(I_SS_CKPT_PATH)
-        if not Path.is_file(SS_PATH):
-            raise Exception(f'Model file does not exist at {SS_PATH}!')
-        i_model_inc = InverseModel.load_from_checkpoint(SS_PATH, strict=False)
-        i_model_inc.eval()
-        with torch.no_grad():
-            y_hat_inc = i_model_inc(x)
-
-        #return ss and inc laser parameter preds
-        return y_hat_ss, y_hat_inc
-
-    def find_in_data(self, y_hat, substrate = 'stainless'):
-        if substrate == 'stainless':
-            params = torch.load(Path(SS_DATA_PATH))["laser_params"]
+    def find_in_data(self, y_hat, substrate = 'steel'):
+        not_in_data = []
+        ss_in_data_same_params = []
+        ss_in_data_diff_params = []
+        if substrate == 'steel':
+            params = self.steel_params
         else:
-            params = torch.load(Path(INC_DATA_PATH))["laser_params"]
-        i = 0
-        found_index = -1
-        for comb in params:
-            i += 1
-            if torch.all(torch.eq(comb, y_hat)):
-                found_index = i
-
-        return y_hat, found_index
+            params = self.inc_params
+        indexes = []
+        for i in range(len(y_hat)):
+            val = torch.all(params == y_hat[i], axis=1).nonzero().flatten()
+            if len(val) == 0:
+                    indexes.append(-1)
+            else:
+                    indexes.append(val.numpy().item()) 
+        return indexes, not_in_data
 
 
     def run_direct(self, y_hat_ss, y_hat_inc, laser_constraint  = False):
-        # if laser_constraint:
-        #     y_hat_ss, ss_index = self.find_in_data(y_hat_ss)
-        #     y_hat_inc, inc_index = self.find_in_data(y_hat_inc)
-        #     if ss_index:
-        #         return y_hat_ss, y_hat_inc
-        #run direct inc model
+
         direct_inc_filepath = Path(D_INC_CKPT_PATH) #CHANGEME
         if not Path.is_file(direct_inc_filepath):
             raise Exception(f'Model file does not exist at {direct_inc_filepath}!')
@@ -123,26 +153,12 @@ class Predictor:
         #retrn 2 emiss predictions
         return direct_ss_emiss_y_hat, direct_inc_emiss_y_hat
 
-    def denormalize_decode_result(self, y_hat, max_speed, max_spacing, min_speed, min_spacing):
-        """input: 1,14 tensor
-            output: 1,3 tensor with wattage no longer one hot encoded"""
-        watts = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3]
-
-        watt_arg = torch.argmax(y_hat[0][2:])
-        y_final = torch.empty(3, dtype=torch.float32)
-        y_final[0] = ((max_speed - min_speed) * y_hat[0][0]) + min_speed # TODO call the scale
-        y_final[1] = ((max_spacing-min_spacing) * y_hat[0][1]) + min_spacing # TODO call the scale
-        y_final[2]= watts[watt_arg]
-        if self.round_laser_params:
-            y_final[0] = torch.round(y_final[0], decimals = -1)
-            y_final[1] = torch.round(y_final[1], decimals = 1)
-            y_final[2] = torch.round(y_final[2], decimals = 1)
-        return y_final
 
     def best_predictor(self, include_inconel, include_stainless, direct_ss_emiss_y_hat, direct_inc_emiss_y_hat, desired_emiss):
         substrates = ['inconel', 'stainless']
         best_params = []
         best_rmse = 0
+
         if not include_inconel and not include_stainless:
             raise Exception(f'Neither inconel or stainless was selected!!')
         if (include_inconel and include_stainless and rmse(direct_inc_emiss_y_hat, desired_emiss) < rmse(direct_ss_emiss_y_hat, desired_emiss)) or (include_inconel and not include_stainless):
@@ -156,6 +172,9 @@ class Predictor:
         
         best_params = [torch.round(best_params[0], decimals = 1).item(), torch.round(best_params[1], decimals = 1).item(), torch.round(best_params[2], decimals = 1).item()]
         return best_substrate, best_params, best_rmse
-    def rmse_metrics(rmse_scores):
-        
-        return mean(rmse_scores), stdev(rmse_scores)
+    
+    def rmse_metrics(y_hat, y_pred):
+
+        rmse_scores = [rmse(y_hat[i], y_pred[i]) for i in range(len(y_hat))]
+
+        return np.mean(rmse_scores), np.stdev(rmse_scores)
