7e0237a24636a838afd1f5acae91f0dd9d8d06d4
pmotameni <47757728+pmotameni@users.noreply.github.com>
2022-07-30 08:18:36 -0700

    ann-set

--- src/config.py
+++ src/config.py
@@ -12,8 +12,8 @@ class Config(BaseModel):
     data_file = "stainless-onehot.pt"
     data_folder: str = "local_data"  # Path to the data folder
     data_portion: float = 1  # Percentage of data being used in the (0 - 1]
-    direction: str = "both"  # direct, inverse, both
-    direct_arch = "res-ann"  # options 'ann', 'res-ann', 'cnn, 'mlpmixer'
+    direction: str = "direct"  # direct, inverse, both
+    direct_arch = "ann"  # options 'ann', 'res-ann', 'cnn, 'mlpmixer'
     direct_batch_size: int = None  # 2**9 512
     direct_gamma: float = .1  # schedular gamma
     direct_lr: float | None = None  # leave default to None
@@ -21,8 +21,8 @@ class Config(BaseModel):
     direct_num_epochs: int = 1600  # default 1600
     # Default= None: It should be under'{work_folder}/saved_best'
     # = "D-0.9-res-ann-inconel.ckpt"  # Default None
-    direct_saved_ckpt: str | None = 'D-1-res-ann-stainless.ckpt'
-    inverse_arch = "res-ann"   # options 'ann', 'res-ann', 'cnn, 'mlpmixer'
+    direct_saved_ckpt: str | None  # = 'D-1-res-ann-stainless.ckpt'
+    inverse_arch = "ann"   # options 'ann', 'res-ann', 'cnn, 'mlpmixer'
     inverse_batch_size: int = None  # 2**9 512
     inverse_gamma: float = .1  # schedular gamma
     inverse_lr: float = None  # tune.loguniform(1e-6, 1e-5)
@@ -31,9 +31,9 @@ class Config(BaseModel):
     enable_early_stopper: bool = True  # when 'True' enables early stopper
     # Default= None: It should be under'{work_folder}/saved_best'
     # str | None =  # Default: None
-    inverse_saved_ckpt: str | None = 'I-1-res-ann-stainless.ckpt'
-    load_direct_checkpoint: bool = True
-    load_inverse_checkpoint: bool = True
+    inverse_saved_ckpt: str | None  # = 'I-1-res-ann-stainless.ckpt'
+    load_direct_checkpoint: bool = False
+    load_inverse_checkpoint: bool = False
     num_gpu: int = 1  # number of GPU
     # TODO: Fix num_wavelens be set at load time
     num_wavelens: int | None = 800  # This will be set @ load time. ex. 800
